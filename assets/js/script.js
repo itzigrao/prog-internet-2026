6f531ab23f0f0f01c566518a4a2c15b4d0b9e969
itzigrao <igorperrot006@gmail.com>
2026-04-16 21:47:48 -0300

    aula 16-04-2026

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // alert('Javascript is working!');
 // const myHeading = document.querySelector('h1');
 // myHeading.textContent = 'Hello world!';
-const form = document.querySelector('form');
+const form = document.querySelector('form');// seleciona o elemento de formulário usando querySelector e armazena em uma variável chamada form
 
 form.addEventListener("submit", function(event){ // criando função para o evento de submit do formulário
     event.preventDefault(); // previne o comportamento padrão do formulário, que é enviar os dados e recarregar a página
@@ -9,7 +9,7 @@ form.addEventListener("submit", function(event){ // criando função para o even
 
 
     const nome = document.getElementById('nome').value; // obtém o valor do campo de nome e decodifica qualquer caractere special
-    // const email = document.getElementById('email').value; // obtém o valor do campo de email e decodifica qualquer caractere special
+    const email = document.getElementById('email').value; // obtém o valor do campo de email
    
     console.log(nome); // exibe o valor do campo de nome no console
     console.log(email); // exibe o valor do campo de email no console
@@ -25,7 +25,9 @@ form.addEventListener("submit", function(event){ // criando função para o even
         alert ('Email invalido. O email deve conter um "@"');
         return;
     }
-})
+
+    alert('Formulário enviado com sucesso!');
+});
 
 document.getElementById('btnAlerta').addEventListener('click', function() {// adiciona um evento de clique ao botão de alerta
     alert('Botão de alerta clicado!');// exibe um alerta quando o botão de alerta é clicado
@@ -36,7 +38,7 @@ document.getElementById('btnCor').addEventListener('click', function() { // adic
 
     document.body.style.backgroundColor = 'grey'; // muda a cor de fundo da página para azul escuro quando o botão de mudar cor é clicado
 
-});;
+});
 
 document.getElementById("btnConteudo").addEventListener('click', function() {
     document.getElementById("conteudo").innerHTML = "Texto alterado diretamente pelo JavaScript!";
